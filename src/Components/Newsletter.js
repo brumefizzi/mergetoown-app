655a6a837da6c20561b933f935d5256325d12559
Brume Ikogho <brumethedev@gmail.com>
2023-05-25 13:14:48 -0400

    edited the footer and newsletter

--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
 
+
 export const Newsletter = ({ status, message, onValidated }) => {
   const [email, setEmail] = useState('');
 
